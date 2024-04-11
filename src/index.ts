1084c31ab22af4d76f6be42ef016b108005bd2a6
venuvarrey <venu.varrey@unifynd.com>
2024-04-12 04:17:54 +0530

    webhook

--- src/index.ts
+++ src/index.ts
@@ -25,7 +25,7 @@ try {
 
   dotenv.config();
 
-  const PORT = process.env.PORT || 5000;
+  const PORT = process.env.PORT || 5001;
 
   const app = express();
   var bodyParser = require('body-parser');
